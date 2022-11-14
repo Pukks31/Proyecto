12c544ae50634c16e89a8e3d7b83a5b758edff04
Pukks31 <mateovillamizar54@gmail.com>
2022-11-14 17:49:28 -0500

    ProyectoFinal

--- Assets/Menu/VolverInicio.cs
+++ Assets/Menu/VolverInicio.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,12 @@ using UnityEngine.SceneManagement;
 
 public class VolverInicio : MonoBehaviour
 {
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
     public void Inicio()
     {
         SceneManager.LoadScene("Menu");
--- Assets/Personaje/Scripts/Colision.cs
+++ Assets/Personaje/Scripts/Colision.cs
@@ -9,7 +9,7 @@ public class Colision : MonoBehaviour
     {
         if(other.gameObject.tag == "Player")
         {
-            SceneManager.LoadScene("juego");
+            SceneManager.LoadScene("Perdio");
         }
     }
 }
